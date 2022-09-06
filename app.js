490210aafb9b3792cf2542089f0461f766ae471b
JeongAnNa <rib040211@gmail.com>
2022-09-06 18:11:00 +0900

    [Update] GET method API

--- app.js
+++ app.js
@@ -18,4 +18,45 @@ app.get('/', (req, res) => {
     res.send("Hello, Restful API Example Page!");
 });
 
+// Request Data: N, Response Date: Y
+app.get('/api/users', (req, res) => {
+    // Retuen User data
+    res.json({ok: true, users: users});
+});
+
+/**
+ * Request Data: Y, Response Date: Y
+ * @path {GET} http://localhost:3000/api/users/user?id=1
+ */
+app.get("/api/users/user", (req, res) => {
+    const user_id = req.query.id;
+    const user = users.filter(data => data.id == user_id);
+
+    res.json({ok: false, user: user});
+});
+
+/**
+ * Request Data: Y, Response Date: Y
+ * @path {GET} http://localhost:3000/api/users/user-body
+ */
+app.get("/api/users/user-body", (req, res) => {
+    const user_id = req.body.id;
+    const user =  users.filter(data => data.id == user_id);
+
+    res.json({ok: false, user: user});
+});
+
+/**
+ * Request Data: Y, Response Date: Y
+ * @path {GET} http://localhost:3000/api/users/:user_id
+ * :user_id == variable
+ */
+app.get("/api/users/:user_id", (req, res) => {
+    const user_id = req.params.user_id;
+    const user = users.filter(data => data.id == user_id);
+
+    res.json({ok: true, user: user})
+})
+
+
 app.listen(port, () => console.log("Listen on " + port + " Port!!"));
